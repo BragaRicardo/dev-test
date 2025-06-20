2447e55bdcdc3add5b16b26d15bae87755b194c6
Ricardo <bragaricardo2022@gmail.com>
2025-06-20 01:07:21 -0300

    Update

--- devtest-web/src/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="es">
-      <body>{children}</body>
-    </html>
-  );
-}
