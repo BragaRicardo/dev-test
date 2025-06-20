24b6d0f65ee698305ff6a62843849b869d47122b
Ricardo <bragaricardo2022@gmail.com>
2025-06-20 00:51:34 -0300

    agg Layout

---
+++ DevTest-web/src/app/usuarios/editar/layout.tsx
@@ -0,0 +1,3 @@
+export default function EditarUsuarioLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
---
+++ DevTest-web/src/app/usuarios/nuevo/layout.tsx
@@ -0,0 +1,3 @@
+export default function NuevoUsuarioLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
